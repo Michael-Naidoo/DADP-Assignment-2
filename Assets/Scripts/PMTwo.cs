ff8a74b947bd70ba483f4816607fe28659e1520c
Michael-Naidoo <130663291+Michael-Naidoo@users.noreply.github.com>
2023-08-16 15:40:20 +0200

    pmtwo

---
+++ Assets/Scripts/PMTwo.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PMTwo : MonoBehaviour
+{
+    public GameObject player;
+    public bool isMoving;
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
+    public float speed = 10;
+    public GameObject wallPiece;
+    public GameObject pos;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            isMoving = true;
+            up = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            isMoving = true;
+            left = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            isMoving = true;
+            down = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            isMoving = true;
+            right = true;
+        }
+
+        var trueSpeed = speed * Time.deltaTime;
+        var position = player.transform.position;
+        if (isMoving && up)
+        {
+            player.transform.position = new Vector3(position.x,position.y + trueSpeed, position.z);
+        }
+        else if (isMoving && down)
+        {
+            player.transform.position = new Vector3(position.x,position.y - trueSpeed, position.z);
+        }
+        else if (isMoving && left)
+        {
+            player.transform.position = new Vector3(position.x - trueSpeed,position.y, position.z);
+        }
+        else if (isMoving && right)
+        {
+            player.transform.position = new Vector3(position.x + trueSpeed,position.y, position.z);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col);
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            isMoving = false;
+            up = false;
+            down = false;
+            left = false;
+            right = false;
+        }
+        else if (col.gameObject.CompareTag("ObsWall") || col.gameObject.CompareTag("Enemy") || col.gameObject.CompareTag("Projectile") || col.gameObject.CompareTag("PitFall"))
+        {
+            isMoving = false;
+            up = false;
+            down = false;
+            left = false;
+            right = false;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.GetComponent<WallPiece>())
+        {
+            Debug.Log(col);
+            Instantiate(wallPiece, pos.transform.position, Quaternion.identity);
+            col.gameObject.SetActive(false);
+            // add vert to the next pos in the game object array for positions of the wall pieces
+            // to do this i need a prefab i can clone
+        }
+        else if (col.gameObject.CompareTag("Finish"))
+        {
+            if (SceneManager.GetActiveScene().name == "Level 1")
+            {
+                SceneManager.LoadScene("Level 2");
+            }
+            else if (SceneManager.GetActiveScene().name == "Level 2")
+            {
+                SceneManager.LoadScene("Level 3");
+            }
+            else if (SceneManager.GetActiveScene().name == "Level 3")
+            {
+                SceneManager.LoadScene("Level 4");
+            }
+            else if (SceneManager.GetActiveScene().name == "Level 4")
+            {
+                SceneManager.LoadScene("Level 1");
+            }
+        }
+    }
+    // exit script
+}
